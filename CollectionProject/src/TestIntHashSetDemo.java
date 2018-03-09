e5e3b56ff75155645f0d051b69fdb984d22333d3
Poorwak <37167283+Poorwak@users.noreply.github.com>
2018-03-09 17:51:03 +0530

    Add files via upload

---
+++ CollectionProject/src/TestIntHashSetDemo.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+
+
+public class TestIntHashSetDemo 
+{
+
+	public static void main(String[] args) 
+	{
+		HashSet<Integer> intSet= new HashSet<Integer>();
+		
+		
+
+	}
+
+}
